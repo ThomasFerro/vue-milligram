32c19763eacfcb4289807ee3a068913bdc843b79
Thomas Ferro <t.ferro184@gmail.com>
2019-02-28 18:37:57 +0100

    Feat (#13) : List component

---
+++ src/components/List.vue
@@ -0,0 +1,37 @@
+<template>
+  <transition-group name="list" :tag="listElement">
+    <component
+      :is="itemElement"
+      v-for="(item, index) in items"
+      :key="getItemKey(item, index)"
+    >{{ item }}
+    </component>
+  </transition-group>
+</template>
+
+<script>
+export default {
+  name: 'vue-milligram-list',
+  props: {
+    listElement: {
+      type: String,
+      default: 'ul',
+    },
+    itemElement: {
+      type: String,
+      default: 'li',
+    },
+    items: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+  methods: {
+    getItemKey(item, index) {
+      return item || index;
+    },
+  },
+};
+</script>
