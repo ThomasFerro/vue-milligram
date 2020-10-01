b72b23673b44c6f03b9e0758edb733d54e3e66a3
Dooxe59 <anthony@ruelle.fr>
2020-10-01 21:13:10 +0200

    chore: Migrate to Vue 3

--- src/main.js
+++ src/main.js
@@ -1,13 +1,10 @@
-import Vue from 'vue';
-import App from './App.vue';
-import VueMilligram from './vue-milligram.plugin';
+import { createApp } from "vue";
+import App from "./App.vue";
+import VueMilligram from "./vue-milligram.plugin";
 
-require('../node_modules/milligram/dist/milligram.min.css');
+require("../node_modules/milligram/dist/milligram.min.css");
 
-Vue.config.productionTip = false;
+const app = createApp(App);
+app.use(VueMilligram);
 
-Vue.use(VueMilligram);
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app');
+app.mount("#app");
